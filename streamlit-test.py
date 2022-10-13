ef648e8659927e36a703848eb4f8e33bcefa4bd0
ramiwbs <107427143+ramiwbs@users.noreply.github.com>
2022-10-13 19:48:28 +0200

    Update streamlit-test.py

--- streamlit-test.py
+++ streamlit-test.py
@@ -6,19 +6,14 @@ import pandas as pd
 model = pickle.load(open('models/trained_pipe_lr.sav', 'rb'))
  
 
-LotArea = st.number_input("Lot Area")
-TotalBsmtSF = st.number_input("Basement Square Feet")
-BedroomAbvGr = st.number_input("Number of Bedrooms")
-GarageCars = st.number_input("Car spaces in Garage")
- 
-import pandas as pd
 new_house = pd.DataFrame({
-    'LotArea':[LotArea],
-    'TotalBsmtSF':[TotalBsmtSF], 
-    'BedroomAbvGr':[BedroomAbvGr], 
-    'GarageCars':[GarageCars]
+    'LotArea':[9000],
+    'TotalBsmtSF':[1000], 
+    'BedroomAbvGr':[5], 
+    'GarageCars':[4]
 })
  
 prediction = model.predict(new_house)
  
 st.write("The price of the house is:", prediction)
+
